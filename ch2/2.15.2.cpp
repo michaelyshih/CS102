94aa095161e3808bf360d1fa3fee65e01781ea71
shihyulung <shihyulung@yahoo.com.tw>
2025-02-07 23:23:45 +0800

    2.15.2

---
+++ ch2/2.15.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string professorName;
+   string courseName;
+
+   /* Your code goes here */
+
+   cout << professorName << endl;
+	cout << courseName << endl;
+
+   return 0;
+}
